c1232a9e0b4817b57986896c004af7df563cd74f
PerAdotz <simoperaotto@gmail.com>
2024-10-31 12:20:10 +0100

    ex3 REST

---
+++ class exercises/02_REST/ex3Cherry.py
@@ -0,0 +1,30 @@
+import requests
+
+print('Available commands:')
+print('all:      Return all the json')
+print('devices:  Return all the devices')
+print('houses:   Return all the houses')
+print('users:    Return all the users')
+print('quit:     Exit')
+
+cmd_list = ['all','devices', 'houses','users']
+cmd = input("Choose the command: ")
+if cmd.strip().lower() != 'quit' :
+    if  cmd.strip().lower() in cmd_list :
+        API = f'https://catalog-p4iot.onrender.com/{cmd}'
+        r = requests.get(API)
+        if r.status_code == 200:
+            json_cnt = r.json()
+            print(json_cnt)
+            #oppure
+            """
+            cnt = r.content.decode('utf-8')
+            json_cnt = json.loads(cnt)
+            print(json_cnt)
+            """
+        else:
+            print(f'Error {r.status_code} fetching data')
+    else:
+        print('Command not available')
+else:
+    print('End')
